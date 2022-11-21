cf282499f0ca79bc1aa4df6b4937233d6d91c23d
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-21 20:39:49 +0900

    Add: application.rbにてRailの基準時刻とDBの基準時刻を変更

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,9 @@ module Mypalette
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.generators.system_tests = nil
   end
 end
