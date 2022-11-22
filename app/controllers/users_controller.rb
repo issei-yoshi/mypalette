7206acb70e0ee00d65c0757d1082619a1c2f227a
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-22 14:48:33 +0900

    Add: users_controllerにてcreateアクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    binding.pry
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :email, :password, :password_confirmation)
   end
 end
