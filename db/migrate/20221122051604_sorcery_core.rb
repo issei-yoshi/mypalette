35ea9e18ef4f714ca1f98a785d609d6e940e9ffb
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-22 14:20:30 +0900

    Add: db:migrateでテーブルを作成

--- db/migrate/20221122051604_sorcery_core.rb
+++ db/migrate/20221122051604_sorcery_core.rb
@@ -4,6 +4,8 @@ class SorceryCore < ActiveRecord::Migration[6.1]
       t.string :email,            null: false, index: { unique: true }
       t.string :crypted_password
       t.string :salt
+      t.string :first_name, null: false
+      t.string :last_name, null: false
 
       t.timestamps                null: false
     end
