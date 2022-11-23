85dcbe30edb31ca73f43d69cf6007d4604e8b7c4
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-23 17:52:58 +0900

    Fix&Add: routesのtypoを修正・ログイン後画面にてログアウト用のリンクを設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
-  delete 'logout' to: 'user_sessions#destroy'
+  delete 'logout', to: 'user_sessions#destroy'
 end
