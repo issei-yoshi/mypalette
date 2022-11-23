f5c8ec2a988a683eefdc55335224856413ce1da5
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-23 17:25:33 +0900

    Add: user_sessions_controllerにてcreateアクションを定義

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,5 +1,14 @@
 class UserSessionsController < ApplicationController
 
   def new ; end
-  
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to root_path
+    else
+      render :new
+    end
+  end
+
 end
