a7a819efeec5dfdd1b2cf2c8ac60df10e6cd5b67
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-23 17:27:18 +0900

    Add: user_sessions_controllerのcreateアクション成功時・失敗時のフラッシュメッセージを設定

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,8 +5,10 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
+      flash[:success] = "ログイン成功しました"
       redirect_back_or_to root_path
     else
+      flash.now[:danger] = "ログイン失敗しました"
       render :new
     end
   end
