66d66d2289b4f6b7d1acc5724ea4b6bfc8370b15
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-23 21:06:54 +0900

    Add: routesにpassword_resets用のルーティングを記載

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "about" => "homes#about"
 
   resources :users, only: [:new, :create]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
