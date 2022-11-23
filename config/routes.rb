4c42a06ed09911cfcdb82e38098c9a5896d2bbcf
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-23 15:52:37 +0900

    Add: rootのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "homes#about"
   get "about" => "homes#about"
 
   resources :users, only: [:new, :create]
