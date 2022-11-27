32ec417b256e3f0f1f1a61d8ac67cb435ea7ec97
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-27 15:39:44 +0900

    Add: static_pagesコントローラにtopアクションを定義し、topページが表示されるように実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  root "homes#about"
+  root "static_pages#top"
   get "about" => "homes#about"
   get "terms_of_use" => "homes#terms"
   get "privacy_policy" => "homes#policy"
