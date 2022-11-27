9d894887de4aabdf6b1c0ed25a07c4e48fd9b633
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-27 12:27:19 +0900

    Add: adminをnamespaceとするルーティングを記載

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  namespace :admin do
+    root to: 'dashboards#index'
+  end
 end
