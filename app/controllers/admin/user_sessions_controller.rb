4d4b4d73e83446e7064193b8fd69d84a69c7b029
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-27 12:48:47 +0900

    Add: admin用のログアウトアクションを定義

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -13,4 +13,10 @@ class Admin::UserSessionsController < Admin::BaseController
     end
   end
 
+  def destroy
+    logout
+    flash[:success] = "adminログアウト成功"
+    redirect_to admin_root_path
+  end
+
 end
