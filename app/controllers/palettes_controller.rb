a848b68862b1f2f5f426659e01225f64ef5a335d
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-28 14:49:52 +0900

    Add: 作成したformからpalettesテーブルに値が保存されるようにcreateアクションを定義

--- app/controllers/palettes_controller.rb
+++ app/controllers/palettes_controller.rb
@@ -3,8 +3,23 @@ class PalettesController < ApplicationController
     @palette = Palette.new
   end
 
-
+  def create
+    @palette = Palette.new(palette_params)
+    if @palette.save
+      flash[:success] = "カラー作成成功しました"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "カラー作成失敗しました"
+      render :new
+    end
+  end
 
   def show
+  end
+
+  private
+
+  def palette_params
+    params.require(:palette).permit(:main, :sub, :body)
   end
 end
