03b0dc0fc0e6e8aa1cc84c70d8eef0258499ecbd
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-28 18:18:15 +0900

    Add: idを付与してcolorchange.jsでDOM操作できるか確認

--- app/javascript/packs/colorchange.js
+++ app/javascript/packs/colorchange.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
+  function clickColor(){
+    console.log('hello');
+  }
 
+  const main = document.querySelector('#main-color');
+
+  main.addEventListener('click', clickColor);
 })
