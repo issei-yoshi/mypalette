72bb0d0872ea626e3936dc2ceb08cfb75c4e8bf4
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-29 20:36:01 +0900

    Add: initial_sub_change.jsを作成して読み込まれた段階で初期カラーを要素に設定するよう実装

---
+++ app/javascript/packs/initial_sub_change.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function (e) {
+
+  const sub = document.querySelector('.sub-color');
+  let subChangeEls = document.querySelectorAll('.sub-change');
+  let subChangeElBgs = document.querySelectorAll('.sub-change-bg');
+
+  const colorVal = sub.value
+
+  subChangeEls.forEach(function(el) {
+    el.style.color = colorVal
+  })
+
+  subChangeElBgs.forEach(function(el) {
+    el.style.backgroundColor = colorVal
+
+    let rgbVal = colorVal.slice(1);
+    const red = rgbVal.substr( 0, 2 );
+    const green = rgbVal.substr( 2, 2 );
+    const blue = rgbVal.substr( 4, 2 );
+    const brightness = Math.floor((parseInt(red, 16) * 0.299) + (parseInt(green, 16) * 0.587) + (parseInt(blue, 16) * 0.114))
+    const textColor = brightness >= 140 ? '#000000' : '#FFFFFF'
+    // console.log(textColor);
+
+    el.style.color = textColor;
+  })
+})
