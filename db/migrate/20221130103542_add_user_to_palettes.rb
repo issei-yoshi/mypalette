3e394df38e374059a00df740b0e8ce6f9bcfcfa1
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-30 19:51:04 +0900

    Add: Palettesテーブルに外部キーを持ったuserカラムを追加

--- db/migrate/20221130103542_add_user_to_palettes.rb
+++ db/migrate/20221130103542_add_user_to_palettes.rb
@@ -1,4 +1,5 @@
 class AddUserToPalettes < ActiveRecord::Migration[6.1]
   def change
+    add_reference :palettes, :user, null: false, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_28_051728) do
+ActiveRecord::Schema.define(version: 2022_11_30_103542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2022_11_28_051728) do
     t.string "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_palettes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +42,5 @@ ActiveRecord::Schema.define(version: 2022_11_28_051728) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
+  add_foreign_key "palettes", "users"
 end
