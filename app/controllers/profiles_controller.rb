1360d46f4d28e8921e4019ee362611fbb018355b
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-30 21:22:15 +0900

    Add: profilesコントローラにupdateアクションを定義

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,5 +8,13 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      flash[:success] =  '編集しました'
+      redirect_back fallback_location:root_path
+    else
+      flash.now[:danger] = '編集に失敗しました'
+      render :new
+    end
   end
 end
