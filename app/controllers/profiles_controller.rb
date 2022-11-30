c608f017a7a12f53e32ea4e11541024e8d324ce4
issei-yoshi <iyoshi.engineer@gmail.com>
2022-11-30 21:23:36 +0900

    Add: privateメソッドのuser_paramsを定義

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -17,4 +17,10 @@ class ProfilesController < ApplicationController
       render :new
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
 end
