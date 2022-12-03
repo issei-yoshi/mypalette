f0f1a8959563ce5fce5034ded50511855efbdb88
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-03 16:08:53 +0900

    Add: colorsコントローラにてupdateアクションを定義

--- app/controllers/admin/colors_controller.rb
+++ app/controllers/admin/colors_controller.rb
@@ -11,6 +11,14 @@ class Admin::ColorsController < Admin::BaseController
   end
 
   def update
+    @palette = Palette.find(params[:id])
+    if @palette.update(palette_params)
+      flash[:success] = "カラー編集に成功しました"
+      redirect_to admin_colors_path
+    else
+      flash.now[:danger] = "カラー編集に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
