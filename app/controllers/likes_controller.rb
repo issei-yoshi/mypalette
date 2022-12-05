8fd23b8fcc5344bfe7eabb2c693226635e26413f
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-05 20:52:10 +0900

    Add: likesコントローラのcreateアクションを実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,8 @@
 class LikesController < ApplicationController
   def create
+    palette = Palette.find(params[:palette_id])
+    current_user.like(palette)
+    redirect_back fallback_location: palette_path
   end
 
   def destroy
