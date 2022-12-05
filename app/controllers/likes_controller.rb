e69f3489ed990b7ec0c3835a005dcbf5d2d76e53
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-05 21:17:36 +0900

    Add: likesコントローラを一部修正・createアクションへ遷移するボタンを_like_buttonパーシャルに追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def create
     palette = Palette.find(params[:palette_id])
     current_user.like(palette)
-    redirect_back fallback_location: palette_path
+    redirect_back fallback_location: palette_path(palette)
   end
 
   def destroy
