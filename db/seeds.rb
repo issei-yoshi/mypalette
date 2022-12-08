5fa2eaa49a162768ecbe42e0c8109c219eb71664
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-08 22:43:35 +0900

    Add: seeds.rbに追加したいTagを記述してrails db:seedコマンドでdbに登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Tag.create([
+  { name: 'Red' },
+  { name: 'Blue' },
+  { name: 'Yellow' },
+  { name: 'Green' },
+  { name: 'Orange' },
+  { name: 'Pink' },
+  { name: 'Purple' },
+  { name: 'White' },
+  { name: 'Black' }
+])
