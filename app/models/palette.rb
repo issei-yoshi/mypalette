3fd2e9f0d6d1c06e954ef8562a925bb4a11c370b
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-08 22:40:28 +0900

    Add: Paletteモデルにpalette_tagモデルとtagモデルとのアソシエーションを定義

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -2,6 +2,8 @@ class Palette < ApplicationRecord
 
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :palette_tags, dependent: :destroy
+  has_many :tags, through: :palette_tags
 
   with_options presence: true do
     validates :main
