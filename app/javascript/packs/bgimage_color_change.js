a68a0dba335ff1f45a02de8e5a407bcca5731a10
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-10 22:46:39 +0900

    Add: bgimage_color_change.jsにてaddEventListenerを設定してbgimageカラムのvalueによって色が変わるよう実装

--- app/javascript/packs/bgimage_color_change.js
+++ app/javascript/packs/bgimage_color_change.js
@@ -1,4 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const BgImage = document.querySelector('.bgimage-change');
-  const BgChange = document.querySelector('#bg-change-id');
+  const bgImage = document.querySelector('.bgimage-change');
+  const bgChange = document.querySelector('#bg-change-id');
+
+  bgImage.addEventListener('input', function(e){
+    let bgImageVal = bgImage.value
+    // console.log(bgImageVal)
+    if (bgImageVal == "Black"){
+      bgChange.setAttribute('class', 'img-bg-black');
+    } else if(bgImageVal == "White") {
+      bgChange.setAttribute('class', 'img-bg-white');
+    } else if(bgImageVal == "Grey") {
+      bgChange.setAttribute('class', 'img-bg-grey');
+    } else if(bgImageVal == "Red") {
+      bgChange.setAttribute('class', 'img-bg-red');
+    } else if(bgImageVal == "Blue") {
+      bgChange.setAttribute('class', 'img-bg-blue');
+    } else if(bgImageVal == "Green") {
+      bgChange.setAttribute('class', 'img-bg-green');
+    } else if(bgImageVal == "Yellow") {
+      bgChange.setAttribute('class', 'img-bg-yellow');
+    } else if(bgImageVal == "Purple") {
+      bgChange.setAttribute('class', 'img-bg-purple');
+    } else if(bgImageVal == "Orange") {
+      bgChange.setAttribute('class', 'img-bg-orange');
+    } else {
+      bgChange.setAttribute('class', 'img-bg-change');
+    }
+  })
 })
