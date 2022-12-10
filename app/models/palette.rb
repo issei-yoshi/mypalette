7f1391aecd82569ff27e635aac131e879f0a391e
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-10 22:09:57 +0900

    Add: Paletteモデルにbgimageをenumで定義

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -12,4 +12,6 @@ class Palette < ApplicationRecord
   end
 
   scope :with_tag, ->(tag_name) { joins(:tags).where(tags: { name: tag_name }) }
+
+  enum bgimage: { Red: 0, Blue: 1, Green: 2, Yellow: 3, Purple: 4, Orange: 5, Black: 6, Grey: 7, White: 8 }
 end
