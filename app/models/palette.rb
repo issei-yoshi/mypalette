baa9a926be19363ff0d7c0105775064cb5d898ac
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-10 22:51:23 +0900

    Add: paletteモデルのenumに記載しているkeyとvalueの順序を変更

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -13,5 +13,5 @@ class Palette < ApplicationRecord
 
   scope :with_tag, ->(tag_name) { joins(:tags).where(tags: { name: tag_name }) }
 
-  enum bgimage: { Red: 0, Blue: 1, Green: 2, Yellow: 3, Purple: 4, Orange: 5, Black: 6, Grey: 7, White: 8 }
+  enum bgimage: { Black: 0, Grey: 1, White: 2, Red: 3, Blue: 4, Green: 5, Yellow: 6, Purple: 7, Orange: 8 }
 end
