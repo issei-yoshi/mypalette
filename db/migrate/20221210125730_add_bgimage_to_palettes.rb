64e0d5a5079e3f456956c4287962d9d0b220b539
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-10 21:59:56 +0900

    Add: db:migrateコマンドでinteger型のbgimageカラムをPalettesテーブルに追加

--- db/migrate/20221210125730_add_bgimage_to_palettes.rb
+++ db/migrate/20221210125730_add_bgimage_to_palettes.rb
@@ -1,4 +1,5 @@
 class AddBgimageToPalettes < ActiveRecord::Migration[6.1]
   def change
+    add_column :palettes, :bgimage, :integer, default: 0, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_08_133336) do
+ActiveRecord::Schema.define(version: 2022_12_10_125730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2022_12_08_133336) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.integer "bgimage", default: 0, null: false
     t.index ["user_id"], name: "index_palettes_on_user_id"
   end
 
