f3348bccbafb174fc84e02a6b6ae59a71eea4c2f
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-11 22:19:02 +0900

    Add: palettesコントローラのindexアクションにてpageスコープを用いて定義

--- app/controllers/palettes_controller.rb
+++ app/controllers/palettes_controller.rb
@@ -8,7 +8,7 @@ class PalettesController < ApplicationController
       Palette.all
     end
 
-    @palettes = palettes.includes([:tags, :user, :likes]).order(created_at: :desc)
+    @palettes = palettes.includes([:tags, :user, :likes]).order(created_at: :desc).page(params[:page])
     render layout: 'layouts/colorless'
   end
 
