1b2beb9ee09562a206a63a688d3f7edb74cc20d3
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-13 00:56:16 +0900

    Add: db:migrateを実行してカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_10_125730) do
+ActiveRecord::Schema.define(version: 2022_12_12_155447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +56,6 @@ ActiveRecord::Schema.define(version: 2022_12_10_125730) do
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "reset_password_token"
