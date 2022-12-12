66e261b54b974f20f8674524339cb99b99a99ba7
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-13 01:00:01 +0900

    Add: usersコントローラのstrong_parameterにてnameカラムを受け取るよう記載

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:last_name, :first_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
