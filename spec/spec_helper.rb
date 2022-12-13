d37d40c060c3eac3598cdfef69ae3e439c8d75bc
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-13 19:30:37 +0900

    Add: support/capybara.rbにseleniumを使用するよう記述

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,9 +17,6 @@
 require 'capybara/rspec'
 
 RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless
-  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
