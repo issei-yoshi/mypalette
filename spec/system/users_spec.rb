ae0f1e1fc94916abde772c42487539440668e442
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-13 21:13:31 +0900

    Add: users_specにてユーザー新規作成が成功するテストを記載

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :system do
+
+  describe 'ユーザー新規登録' do
+    context 'フォームの入力値が正常' do
+      it 'ユーザーの新規作成が成功する' do
+        visit new_user_path
+        fill_in '名前', with: 'TestUser'
+        fill_in 'メールアドレス', with: 'test@example.com'
+        fill_in "user_password", with: 'password'
+        fill_in "user_password_confirmation", with: 'password'
+        click_button '登録'
+        expect(page).to have_content '登録が完了しました'
+        expect(current_path).to eq root_path
+      end
+    end
+  end
+
+end
