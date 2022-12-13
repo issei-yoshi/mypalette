d9d936bbaba54b804816695e75880b0699617549
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-13 21:23:42 +0900

    Add: users_specにてメールアドレスが未入力により、ユーザー新規作成が失敗するテストを記載

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -15,6 +15,20 @@ RSpec.describe 'Users', type: :system do
         expect(current_path).to eq root_path
       end
     end
+
+    context 'メールアドレスが未入力' do
+      it 'ユーザーの新規作成が失敗する' do
+        visit new_user_path
+        fill_in '名前', with: 'TestUser'
+        fill_in 'メールアドレス', with: ''
+        fill_in "user_password", with: 'password'
+        fill_in "user_password_confirmation", with: 'password'
+        click_button '登録'
+        expect(page).to have_content '登録に失敗しました'
+        expect(page).to have_content "Email can't be blank"
+        expect(current_path).to eq users_path
+      end
+    end
   end
 
 end
