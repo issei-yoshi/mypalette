fc73996b7598d063493ffaa9be3b04bf5a2026af
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-13 21:27:32 +0900

    Add: users_specにてメールアドレスが既に登録されていることにより、ユーザー新規作成が失敗するテストを記載

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
 
+  let(:user) { create(:user) }
+
   describe 'ユーザー新規登録' do
     context 'フォームの入力値が正常' do
       it 'ユーザーの新規作成が成功する' do
@@ -27,6 +29,22 @@ RSpec.describe 'Users', type: :system do
         expect(page).to have_content '登録に失敗しました'
         expect(page).to have_content "Email can't be blank"
         expect(current_path).to eq users_path
+      end
+    end
+
+    context '登録済みのメールアドレスを使用' do
+      it 'ユーザーの新規作成が失敗する' do
+        existed_user = create(:user)
+        visit new_user_path
+        fill_in '名前', with: 'TestUser'
+        fill_in 'メールアドレス', with: existed_user.email
+        fill_in "user_password", with: 'password'
+        fill_in "user_password_confirmation", with: 'password'
+        click_button '登録'
+        expect(page).to have_content '登録に失敗しました'
+        expect(page).to have_content "Email has already been taken"
+        expect(current_path).to eq users_path
+        expect(page).to have_field 'メールアドレス', with: existed_user.email
       end
     end
   end
