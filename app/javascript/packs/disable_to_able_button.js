2588ddf6e5932f8bd3b8514b490053316c895c3d
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-15 02:56:37 +0900

    Add: _formパーシャルのsubmitボタンにdisabledオプションを付与して、全てのパレットの値が変更されない限りsubmitボタンが有効にならないよう実装

--- app/javascript/packs/disable_to_able_button.js
+++ app/javascript/packs/disable_to_able_button.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log('aaa')
+  let main = document.querySelector('.main-color');
+  let sub = document.querySelector('.sub-color');
+  let body = document.querySelector('.body-color');
+  let button = document.querySelector('.submit-button')
+
+  main.addEventListener("input", ()=> {
+    main = "input"
+    change();
+  })
+
+  sub.addEventListener("input", ()=> {
+    sub = "input"
+    change();
+  })
+
+  body.addEventListener("input", ()=> {
+    body = "input"
+    change();
+  })
+
+  function change(){
+    if (main == "input" && sub == "input" && body == "input") {
+      button.disabled = false
+      button.style.backgroundColor = "#34d399"
+      button.style.cursor = "pointer"
+      return
+    }
+  }
 })
