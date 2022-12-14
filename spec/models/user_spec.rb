4c1b1ea44658a3607f8f976d5b77bab0b74a3703
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-14 23:13:57 +0900

    Add: nameカラムが空欄の時にバリデーションが発生するテストを記載

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,13 @@ RSpec.describe 'Userモデルのテスト', type: :model do
   describe 'バリデーションのテスト' do
     context 'emailカラム' do
       it '空欄の場合は作成できない' do
+        user = build(:user, email: '')
+        expect(user.valid?).to eq false;
+      end
+    end
+
+    context 'nameカラム' do
+      it '空欄の場合は作成できない' do
         user = build(:user, name: '')
         expect(user.valid?).to eq false;
       end
