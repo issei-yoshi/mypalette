fe18bd116f08c7c8ae6e0b183ded337c6c1ccaf2
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-28 23:14:25 +0900

    Fix: usersコントローラのコールバック関数を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: %i[new create]
+  skip_before_action :require_login, only: [:new, :create]
   layout 'layouts/colorless'
 
   def new
