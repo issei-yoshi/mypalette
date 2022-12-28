46f480fef86fa9e9238236b644528121b8407786
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-28 23:12:09 +0900

    Fix: usersコントローラのバリデーションメッセージにあった不要なスペースを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] =  '登録が完了しました'
+      flash[:success] = '登録が完了しました'
       redirect_to root_path
     else
       flash.now[:danger] = '登録に失敗しました'
