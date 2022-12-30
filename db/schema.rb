65624f71b9335b4cd54cf21ad0277b64f826f687
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-30 21:49:38 +0900

    Add: db:migrateコマンドでpalette_secondsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_12_155651) do
+ActiveRecord::Schema.define(version: 2022_12_30_124445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2022_12_12_155651) do
     t.index ["palette_id"], name: "index_likes_on_palette_id"
     t.index ["user_id", "palette_id"], name: "index_likes_on_user_id_and_palette_id", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "palette_seconds", force: :cascade do |t|
+    t.string "main", null: false
+    t.string "sub", null: false
+    t.string "body", null: false
+    t.string "bg", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_palette_seconds_on_user_id"
   end
 
   create_table "palette_tags", force: :cascade do |t|
@@ -70,6 +81,7 @@ ActiveRecord::Schema.define(version: 2022_12_12_155651) do
 
   add_foreign_key "likes", "palettes"
   add_foreign_key "likes", "users"
+  add_foreign_key "palette_seconds", "users"
   add_foreign_key "palette_tags", "palettes"
   add_foreign_key "palette_tags", "tags"
   add_foreign_key "palettes", "users"
