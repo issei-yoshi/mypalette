786d03df94e38acbfd1ea9d41bf82caa6dfc9813
issei-yoshi <iyoshi.engineer@gmail.com>
2022-12-30 21:48:51 +0900

    Add: マイグレーションファイルにテーブルに追加したいカラムを記載

--- db/migrate/20221230124445_create_palette_seconds.rb
+++ db/migrate/20221230124445_create_palette_seconds.rb
@@ -1,6 +1,11 @@
 class CreatePaletteSeconds < ActiveRecord::Migration[6.1]
   def change
     create_table :palette_seconds do |t|
+      t.string :main, null: false
+      t.string :sub, null: false
+      t.string :body, null: false
+      t.string :bg, null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
