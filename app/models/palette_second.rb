ed6ae4325ece7c0811da3f222a6924f3219e83eb
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-02 00:28:10 +0900

    Add: palette_secondモデルにwith_tagのscopeを定義

--- app/models/palette_second.rb
+++ app/models/palette_second.rb
@@ -3,4 +3,6 @@ class PaletteSecond < ApplicationRecord
   has_many :like_seconds, dependent: :destroy
   has_many :palette_second_tags, dependent: :destroy
   has_many :tag_seconds, through: :palette_second_tags
+
+  scope :with_tag, ->(tag_second_name) { joins(:tag_seconds).where(tag_seconds: { name: tag_second_name }) }
 end
