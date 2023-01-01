918eac07d3e0b0ebfff7768468632d46c19b5f5a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-02 00:26:00 +0900

    Add: palette_secondモデルにアソシエーションを定義

--- app/models/palette_second.rb
+++ app/models/palette_second.rb
@@ -1,4 +1,6 @@
 class PaletteSecond < ApplicationRecord
   belongs_to :user
   has_many :like_seconds, dependent: :destroy
+  has_many :palette_second_tags, dependent: :destroy
+  has_many :tag_seconds, through: :palette_second_tags
 end
