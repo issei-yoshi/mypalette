b346d111dc889002c5a3a80641c663fbfdf2ebfd
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-02 00:24:11 +0900

    Add: tag_secondモデルにアソシエーションを定義

--- app/models/tag_second.rb
+++ app/models/tag_second.rb
@@ -1,3 +1,6 @@
 class TagSecond < ApplicationRecord
+  has_many :palette_second_tags, dependent: :destroy
+  has_many :palette_seconds, through: :palette_second_tags
+
   validates :name, presence: true, uniqueness: true
 end
