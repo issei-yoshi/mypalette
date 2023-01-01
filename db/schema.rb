272ae96fe9b8938995bdbc56f012e0062994cfec
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-02 00:08:00 +0900

    Add: db:migrateでtag_secondsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_31_023526) do
+ActiveRecord::Schema.define(version: 2023_01_01_150559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,12 @@ ActiveRecord::Schema.define(version: 2022_12_31_023526) do
     t.bigint "user_id", null: false
     t.integer "bgimage", default: 0, null: false
     t.index ["user_id"], name: "index_palettes_on_user_id"
+  end
+
+  create_table "tag_seconds", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tags", force: :cascade do |t|
