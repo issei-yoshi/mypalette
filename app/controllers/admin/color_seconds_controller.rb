567cfaa95a50e016c5c8c9bb5ff260d1daca2e21
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-04 10:01:38 +0900

    Add: admin/color_secondsコントローラに各アクションを定義

--- app/controllers/admin/color_seconds_controller.rb
+++ app/controllers/admin/color_seconds_controller.rb
@@ -1,2 +1,40 @@
 class Admin::ColorSecondsController < Admin::BaseController
+  layout 'admin/layouts/admin_colorless'
+  before_action :set_palette, only: [:edit, :update, :destroy]
+
+  def index
+    @palette_seconds = PaletteSecond.includes(:user).page(params[:page])
+  end
+
+  def show; end
+
+  def edit
+    render layout: 'admin/layouts/application'
+  end
+
+  def update
+    if @palette_second.update(palette_params)
+      flash[:success] = "カラー編集に成功しました"
+      redirect_to admin_colors_path
+    else
+      flash.now[:danger] = "カラー編集に失敗しました"
+      render :edit
+    end
+  end
+
+  def destroy
+    @palette_second.destroy!
+    flash[:success] = "カラーを削除しました"
+    redirect_to admin_colors_path
+  end
+
+  private
+
+  def palette_params
+    params.require(:palette).permit(:main, :sub, :body, :bg, tag_second_ids: [])
+  end
+
+  def set_palette
+    @palette_second = PaletteSecond.find(params[:id])
+  end
 end
