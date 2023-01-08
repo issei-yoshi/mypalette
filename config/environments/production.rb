3b1e42e991cf15947a4215c1f66979f001342d42
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-08 15:41:21 +0900

    Add: production.rbとdevelopment.rbの不要な記述を削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
     port: 587,
-    user_name: Rails.application.credentials.gmail[:user_name],  #Gmailアドレス（credentials.yml.encに記載）
-    password: Rails.application.credentials.gmail[:password],  #アプリパスワード（credentials.yml.encに記載）
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
     authentication: :login
   }
 
