77448ab5847ae1cf1fdd8d02a60967d803f82117
issei-yoshi <iyoshi.engineer@gmail.com>
2023-02-12 13:17:22 +0900

    Fix:palette_secondモデルのバリデーション記載を修正

--- app/models/palette_second.rb
+++ app/models/palette_second.rb
@@ -4,10 +4,12 @@ class PaletteSecond < ApplicationRecord
   has_many :palette_second_tags, dependent: :destroy
   has_many :tag_seconds, through: :palette_second_tags
 
-  validates :main, presence: true
-  validates :sub, presence: true
-  validates :body, presence: true
-  validates :bg, presence: true
+  with_options presence: true do
+    validates :main
+    validates :sub
+    validates :body
+    validates :bg
+  end
 
   scope :with_tag, ->(tag_second_name) { joins(:tag_seconds).where(tag_seconds: { name: tag_second_name }) }
 end
