20a03e4f6d00012bdc34c2b83b71068f9b4aeace
issei-yoshi <iyoshi.engineer@gmail.com>
2023-02-12 13:09:20 +0900

    palette_secondモデルのmainカラムについてバリデーションテストを記載

---
+++ spec/models/palette_second_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Platte_secondモデルのテスト' do
+  describe 'バリデーションテスト' do
+    let(:user) { create(:user) }
+    let(:palette_second) { build(:palette_second, user_id: user.id) }
+
+    context 'mainカラム' do
+      it '空欄の場合は作成できない' do
+        palette_second = build(:palette_second, main: '')
+        expect(palette_second.valid?).to be false
+      end
+    end
+  end
+end
