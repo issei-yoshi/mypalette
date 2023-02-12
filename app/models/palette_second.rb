23e59feaa69365bf2e82cc3f6028d0520006cc26
issei-yoshi <iyoshi.engineer@gmail.com>
2023-02-12 13:14:39 +0900

    palette_secondモデルにbodyカラムのバリデーションを設定

--- app/models/palette_second.rb
+++ app/models/palette_second.rb
@@ -6,6 +6,7 @@ class PaletteSecond < ApplicationRecord
 
   validates :main, presence: true
   validates :sub, presence: true
+  validates :body, presence: true
 
   scope :with_tag, ->(tag_second_name) { joins(:tag_seconds).where(tag_seconds: { name: tag_second_name }) }
 end
